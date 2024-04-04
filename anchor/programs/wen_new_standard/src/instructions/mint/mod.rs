a55feaf697e5a98aa80c28a8d70bf4530620e910
Sundeep Charan Ramkumar <webdevdesign@sundeepcharan.com>
2024-04-04 06:16:29 +0530

    chore: added wns program locally

---
+++ anchor/programs/wen_new_standard/src/instructions/mint/mod.rs
@@ -0,0 +1,14 @@
+pub mod burn;
+pub mod create;
+pub mod freeze;
+pub mod thaw;
+
+pub mod group;
+pub mod metadata;
+
+pub use burn::*;
+pub use create::*;
+pub use freeze::*;
+pub use group::*;
+pub use metadata::*;
+pub use thaw::*;
---
+++ anchor/programs/wen_new_standard/src/instructions/mod.rs
@@ -0,0 +1,7 @@
+pub mod group;
+pub mod manager;
+pub mod mint;
+
+pub use group::*;
+pub use manager::*;
+pub use mint::*;
